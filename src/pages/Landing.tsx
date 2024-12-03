3866274c4e74850b6f6645d16ab4317ce8e1d86d
Kaito <sekhsayan0102@gmail.com>
2024-12-03 17:34:50 +0530

    Update Landing.tsx

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -429,7 +429,7 @@ const Landing = () => {
         <div className="mt-16 border-t border-gray-800">
           <div className="py-8">
             <div className="text-center text-gray-500 text-sm">
-              &copy; AnixPlay.xyz. All rights reserved.
+              &copy; 2025 AnixPlay.xyz. All rights reserved.
             </div>
           </div>
         </div>
